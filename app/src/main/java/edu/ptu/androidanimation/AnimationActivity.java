d8c844985055012163fd1059fd0e17df50c3a789
Rick Smith <wangash@qq.com>
2016-02-27 22:41:10 +0800

    添加资源

---
+++ app/src/main/java/edu/ptu/androidanimation/AnimationActivity.java
@@ -0,0 +1,28 @@
+package edu.ptu.androidanimation;
+
+import android.animation.Animator;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import edu.ptu.androidanimation.animation.AnimationFactory;
+
+public class AnimationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animation);
+    }
+    public void playAnim(View tvLabel){
+        AnimationFactory animationFactory = new AnimationFactory();
+//        Animation viewAnimation = animationFactory.createViewAnimation();
+//        tvLabel.setAnimation(viewAnimation);
+//        viewAnimation.setStartOffset(5000);
+//        AnimationDrawable animationDrawable = animationFactory.createAnimationDrawable(this);
+//        tvLabel.setBackgroundDrawable(animationDrawable);
+//        animationDrawable.start();
+        Animator propertyAnimation = animationFactory.createPropertyAnimation(tvLabel, "alpha");
+        propertyAnimation.start();
+    }
+}
